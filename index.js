232975f84aa51f973084fe7079dda15b1b36b74b
Dustin Astrin <dustastrin@gmail.com>
2017-11-07 17:02:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your code in this file!
+function scuberGreetingForFeet(x) {
+  if (x<=400){ return "This one is on me!"}
+  else if (x>=2000 && x<=2500) {return "I will gladly take your thirty bucks."}
+  else if (x>2500) {return "No can do."}
+}
+
+function ternaryCheckCity(x) {
+return (x === `NYC`) ? "Ok, sounds good." : `No go.`;
+}
+
+function switchOnCharmFromTip(Tip) {
+switch (Tip) {
+  case 'generous' :
+  return "Thank you so much."
+  break;
+
+  case 'not as generous' :
+  return "Thank you."
+  break;
+
+  default :
+  return "Bye."
+  break;
+}
+
+
+}
