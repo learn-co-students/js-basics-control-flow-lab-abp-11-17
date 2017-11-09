56587a97f556f489ef4ca2c52c2c800e72118762
Matthew Garrett <mdgarrett1984@gmail.com>
2017-11-09 23:51:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code in this file!
+let distance;
+
+function scuberGreetingForFeet() {
+  if (distance <= 400) {
+    console.log('This one is on me!');
+  } else if (distance > 2000) {
+    console.log('I will gladly take your thirty bucks.');
+  } else if (distance > 2500) {
+    console.log('No can do');
+  }
+}
+
+function ternaryCheckCity() {}
+
+function switchOnCharmFromTip() {}
