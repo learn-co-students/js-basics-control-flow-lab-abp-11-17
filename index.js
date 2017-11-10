dac0f931b0bde50bbc0c33fd8bf136b4e113b9e1
Carolyn Yuan <carolynlyuan@gmail.com>
2017-11-10 17:08:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your code in this file!
+function scuberGreetingForFeet(distance) {
+  if (distance <= 400) {
+    return 'This one is on me!';
+  }
+  else if (distance > 2000 && distance < 2500) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  else {
+    return 'No can do.';
+  }
+}
+
+function ternaryCheckCity(city) {
+  return (city === 'NYC') ? 'Ok, sounds good.' : 'No go.';
+}
+
+function switchOnCharmFromTip(tip) {
+  return (tip === 'generous') ? 'Thank you so much.' : (tip === 'not as generous') ? 'Thank you.' : 'Bye.';
+}
