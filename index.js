209b1d9a1f433c0c187917f03189870f4e1ff7ea
Dustin Wang <dustinrwang@gmail.com>
2017-11-10 02:01:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your code in this file!
+function scuberGreetingForFeet(distance) {
+  if (distance < 400) {
+    return "This one is on me!";
+  } else if (distance > 2000 && distance < 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (distance > 2500) {
+    return 'No can do.';
+  }
+}
+
+
+function ternaryCheckCity(city) {
+  return city=='NYC' ? 'Ok, sounds good.' : 'No go.';
+}
+
+
+function switchOnCharmFromTip(tipamt) {
+  switch (tipamt) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+
+      case 'not as generous':
+        return 'Thank you.';
+        break;
+
+      case 'thanks for everything':
+        return 'Bye.';
+        break;
+  }
+}
+
+  /* Goal - Does the city = NYC? Yes = return ok, No = return no go
+   1. A => return '
+   2. B => 'No go'
+  */
