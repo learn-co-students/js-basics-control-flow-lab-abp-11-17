24d8a57555746ebfc1dccf959e658e673873b17d
meinnes <innesmercean@gmail.com>
2017-11-10 17:21:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your code in this file!
+function scuberGreetingForFeet(distance){
+  let greet;
+  if (distance<=400){
+    greet="This one is on me!";
+  } else if (distance<=2500 && distance>2000){
+    greet="I will gladly take your thirty bucks.";
+  } else {
+    greet="No can do.";
+  }
+  return greet;
+}
+
+function ternaryCheckCity(city){
+  let response;
+  if (city==="NYC"){
+    response="Ok, sounds good.";
+  } else {
+    response="No go.";
+  }
+  return response;
+}
+
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.';
+    break;
+    case 'not as generous':
+      return 'Thank you.';
+    break;
+    default:
+      return 'Bye.';
+  }
+}
