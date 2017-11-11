3de2509ea1c9e5dcd4f51bbf566b3eecbe63f849
Osbaldo Martinez Jr <martinez.osbaldo@gmail.com>
2017-11-11 23:49:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your code in this file!
+function scuberGreetingForFeet(x){
+  if (x < 400){
+    return ("This one is on me!")
+  } else if (x>2500) {
+    return ("No can do.")
+  } else {
+    return ("I will gladly take your thirty bucks.")
+  }
+}
+
+function ternaryCheckCity(y){
+  x=(y===('NYC')) ? "Ok, sounds good." : "No go."
+  return x
+}
+
+function switchOnCharmFromTip(type){
+  switch (type) {
+    case 'generous':
+      return "Thank you so much."
+    case 'not as generous':
+      return "Thank you."
+    default:
+      return "Bye."
+
+  }
+}
