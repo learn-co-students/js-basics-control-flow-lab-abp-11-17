2d8d1bab7e0f3812469b7bb2b801aced211990c5
Shireen Ghazala <shireen.gaz@gmail.com>
2017-11-11 18:50:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
 
 
-function scuberGreetingForFeet(distance) {
-  If (distance===400 || distance<400){
+
+function scuberGreetingForFeet(feet) {
+  if (feet===400 || feet<400){
     return "This one is on me!"
     }
- elseif(distance>2000 && distance<2500){
+ else if(feet>2000 && feet<2500){
     return "I will gladly take your thirty bucks."
  }
- elseif(distance>2500){
+ else if(feet>2500){
    return "No can do."
  }
 }
 
 function ternaryCheckCity(city) {
-city="NYC"? "Ok, sounds good.":"No Go.";
+ return city==="NYC"? "Ok, sounds good.": "No go.";
 }
 
 function switchOnCharmFromTip(Tip) {
   switch (Tip) {
     case "generous":
       return "Thank you so much.";
-      break;
-    case: "not so generous":
+    break;
+    case "not as generous":
        return "Thank you.";
-       break;
+    break;
     default:
-       return "Bye";
+       return "Bye.";
   }
 
 }
