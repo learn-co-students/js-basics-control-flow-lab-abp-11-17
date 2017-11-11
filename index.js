98ed02a422abeff3164ee2e97053605252ac14c4
Bomi Kim <bkim1289@gmail.com>
2017-11-11 03:10:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your code in this file!
+function scuberGreetingForFeet(feet) {
+  if (feet<=400) {
+    return "This one is on me!";
+  } else if (1999 < feet && feet < 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+  return "No can do.";
+  }
+}
+
+function ternaryCheckCity(str){
+  if (str === 'NYC') {
+    return 'Ok, sounds good.'
+  } else {
+    return 'No go.';
+  }
+}
+
+function switchOnCharmFromTip(str){
+  if (str === 'generous') {
+    return 'Thank you so much.'
+  } else if (str === 'not as generous') {
+    return 'Thank you.'
+  } else {
+    return 'Bye.';
+  }
+}
+
+/*
+function switchOnCharmFromTip (tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
+}
+*/
