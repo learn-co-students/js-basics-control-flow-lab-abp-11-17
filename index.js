4140dd09664fffa7f544db2194ac92b7c288d753
sabu sabu <godugubalesabu@gmail.com>
2017-11-11 20:56:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your code in this file!
+function scuberGreetingForFeet(distance){
+
+  if(distance>2500){
+    return 'No can do.';
+  } else{
+    if(distance<=400){
+      return "This one is on me!";
+    }
+    if(distance>2000){
+      return "I will gladly take your thirty bucks.";
+    }
+  }
+}
+
+function ternaryCheckCity(city){
+  return city==='NYC' ? 'Ok, sounds good.':'No go.';
+
+}
+
+function switchOnCharmFromTip(role){
+  switch(role){
+    case "generous":
+    return 'Thank you so much.';
+    break;
+    case "not as generous":
+    return 'Thank you.';
+    break;
+    case 'thanks for everything':
+    return 'Bye.';
+  }
+}
