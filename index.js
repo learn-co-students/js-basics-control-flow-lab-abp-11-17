57ff0803f26ab00b59fc7152520372d04621e654
afrika.williams@gmail.com <afrika.williams@gmail.com>
2017-11-12 02:19:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your code in this file!
+
+function scuberGreetingForFeet(string) {
+ if (string<=400) {
+   return  "This one is on me!";
+ } else if (string>2000 && string<=2500) {
+   return 'I will gladly take your thirty bucks.';
+ } else if (string>2500) {
+   return 'No can do.';
+ }
+}
+
+function ternaryCheckCity(city) {
+  if (city=== 'NYC') {
+    return "Ok, sounds good.";
+  } else {
+    return "No go.";
+  }
+}
+
+function switchOnCharmFromTip(tip) {
+  if (tip==='generous'){
+    return "Thank you so much.";
+  } else if (tip==='not as generous'){
+    return "Thank you.";
+  } else {
+    return "Bye.";
+  }
+}
