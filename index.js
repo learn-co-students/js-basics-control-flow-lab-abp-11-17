f90e46a6219fd809b73b648129612eb2b1b4afcf
Kaitlyn Manlove <kaitlyn.manlove@me.com>
2017-11-12 20:33:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your code in this file!
+function scuberGreetingForFeet(feet)  {
+  if (feet < 400) {
+    return "This one is on me!"
+  }
+
+  if (feet > 2000 && feet < 2500) {
+    return "I will gladly take your thirty bucks."
+  }
+
+  if (feet > 2500) {
+    return "No can do."
+  }
+}
+
+function ternaryCheckCity(city) {
+  let location
+
+  if (city === "NYC") {
+    return "Ok, sounds good."
+  }
+
+  if (city !== "NYC") {
+    return "No go."
+  }
+}
+
+function switchOnCharmFromTip(tip) {
+  let generous
+
+  switch (tip) {
+    case 'generous':
+      return "Thank you so much."
+      break;
+
+      case 'not as generous':
+        return "Thank you."
+        break;
+
+    default:
+        return "Bye."
+        break;
+  }
+}
