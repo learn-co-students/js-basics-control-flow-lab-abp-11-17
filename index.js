44bde1bd0a465234f93592be078aa4be50ef7475
Patrick Bope <patbpm1@gmail.com>
2017-11-14 07:38:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your code in this file!
+function scuberGreetingForFeet(number){
+  if (number < 400) {
+    return 'This one is on me!'
+  } else if (number > 2000 && number < 2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else if (number > 2500) {
+    return 'No can do.'
+  }
+}
+scuberGreetingForFeet(2501);
+
+function ternaryCheckCity(city){
+  if (city === 'NYC') {
+    return 'Ok, sounds good.'
+  }else {
+    return 'No go.'
+  }
+}
+ternaryCheckCity('NYC');
+
+
+function switchOnCharmFromTip(response){
+
+
+  switch (response) {
+    case 'generous':
+      return 'Thank you so much.'
+    break;
+    case 'not as generous':
+      return 'Thank you.'
+    break;
+    case  'thanks for everything':
+      return 'Bye.'
+
+  }
+}
+switchOnCharmFromTip('generous');
